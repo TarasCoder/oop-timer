9e62a7d40cc6dd821badf31de4839146287c65a8
TarasCoder <66311545+TarasCoder@users.noreply.github.com>
2022-09-30 15:57:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,50 @@ class Stopwatch {
   stopped = false;
 
   start() {
-    this.startTime = new Date();
-    this.startTimeIsSet = true;
-    console.log(this.startTime);
+    if (!this.startTimeIsSet) {
+      this.startTime = new Date();
+      this.startTimeIsSet = true;
+      console.log(this.startTime);
+    }
   }
   stop() {
     this.stopTime = new Date();
     this.stopTimeIsSet = true;
     console.log(this.stopTime);
+    this.stopped = true;
   }
   reset() {
     this.startTime = 0;
     this.stopTime = 0;
     this.stopped = false;
-    this.duration = 0;
-    console.log("Sw was reset...");
+    this.totalDuration = 0;
+    this.startTimeIsSet = false;
+    this.stopTimeIsSet = false;
+    console.log("Stop watch is reset.");
   }
   duration() {
-    if (this.startTimeIsSet === true || this.stopTimeIsSet === true){
-        this.totalDuration = (this.stopTime.getTime() - this.startTime.getTime()) / 1000;
+    if (this.startTimeIsSet === true && this.stopTimeIsSet === true) {
+      this.totalDuration =
+        (this.stopTime.getTime() - this.startTime.getTime()) / 1000;
     }
     console.log("Duration is: " + this.totalDuration);
   }
 }
+let sw = new Stopwatch();
 
-
-
+// Testig section
+setTimeout(() => {
+  sw.start();
+}, 0);
+setTimeout(() => {
+  sw.stop();
+}, 2000);
+setTimeout(() => {
+  sw.duration();
+}, 3000);
+setTimeout(() => {
+  sw.stop();
+}, 6000);
+setTimeout(() => {
+  sw.duration();
+}, 7000);
